4edd18f0725efa0459dd8622d8bcd1234dbdbf4b
Sezen <sezen.ibrahim3@gmail.com>
2022-11-03 16:46:26 +0200

    Clear n Architecture

---
+++ PeezyMovies.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace PeezyMovies.Infrastructure.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ PeezyMovies/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿namespace PeezyMovies.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using PeezyMovies.Models;
+    using System.Diagnostics;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+        
+    
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
