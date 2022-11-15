bf90c7607cee51b111b9f60c005891337916e718
Sezen <sezen.ibrahim3@gmail.com>
2022-11-15 21:35:25 +0200

    Implemented Ajax Demo For Getting actors data

--- PeezyMovies/Controllers/HomeController.cs
+++ PeezyMovies/Controllers/HomeController.cs
@@ -1,11 +1,33 @@
 ﻿namespace PeezyMovies.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using PeezyMovies.Core.Contracts;
+    using PeezyMovies.Infrastructure.Data.Common;
+    using PeezyMovies.Infrastructure.Data.Models;
     using PeezyMovies.Models;
     using System.Diagnostics;
 
     public class HomeController : Controller
     {
+        private readonly IActorService actorService;
+        private readonly IRepository repo;
+
+        public HomeController(IActorService actorService, IRepository repo)
+        {
+            this.actorService = actorService;
+            this.repo = repo;
+        }
+
+        public IActionResult AjaxDemo()
+        {
+            return this.View();
+        }
+
+        public IActionResult AjaxData()
+        {
+            var result = this.repo.All<Actor>().ToList();
+            return this.Json(result);
+        }
         public IActionResult Index()
         {
             if (User?.Identity.IsAuthenticated ?? false)
